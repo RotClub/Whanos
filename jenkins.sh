f08988049c96e6d5f09b76a3c5198679091b1f69
Yanis Moyon <yanis.moyon@epitech.eu>
2024-11-26 10:48:22 +0100

    FIX: use docker-compose for Jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,24 +1,13 @@
 #! /bin/bash
 
 # Set the Jenkins home directory
-export JENKINS_HOME=$PWD/jenkins_home
+export JENKINS_HOME=$PWD/jenkins
 export JENKINS_PORT=8080
+export JENKINS_LOGS=$JENKINS_HOME/logs
 
 # Run Jenkins
-sudo docker run \
-    -p $JENKINS_PORT:8080 \
-    -p 50000:50000 \
-    -v $JENKINS_HOME:/var/jenkins_home \
-    --env JAVA_OPTS="-Djenkins.install.runSetupWizard=false" \
-    --env CASC_JENKINS_CONFIG=/var/jenkins_home/config.yml \
-    --workdir /var/jenkins_home \
-    --name whanos-jenkins \
-    jenkins/jenkins:lts jenkins-plugin-cli --plugins -f /var/jenkins_home/plugins.txt
-
-# Print the Jenkins URL
-echo "Jenkins is now running at http://localhost:$JENKINS_PORT"
-echo "  - Jenkins home directory mounted at $JENKINS_HOME"
-echo "  - To access the Jenkins logs, run: sudo docker logs whanos-jenkins"
-echo "  - To stop Jenkins, run: sudo docker stop whanos-jenkins"
-echo "  - To remove Jenkins, run: sudo docker rm whanos-jenkins"
-echo "  - To restart Jenkins without deleting it, run: sudo docker start whanos-jenkins"
+if sudo docker-compose up; then
+    echo "Jenkins is running on http://localhost:$JENKINS_PORT"
+else
+    echo "Jenkins failed to start"
+fi
